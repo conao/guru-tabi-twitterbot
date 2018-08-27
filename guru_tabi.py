fef5a62756faf66861c8d91b1ced7de126fd178f
Naoya Yamashita <conao3@gmail.com>
2018-08-27 21:34:18 +0900

    implement browsePage

--- guru_tabi.py
+++ guru_tabi.py
@@ -3,6 +3,9 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
+import re
+import pdb
 
 """
 guru_tabi is twitter-bot for guru-tabi(https://gurutabi.gnavi.co.jp/a/).
@@ -13,8 +16,20 @@ def browsePage():
     r = requests.get(target_url)
     soup = BeautifulSoup(r.text, 'lxml')
     panels = soup.find_all("li", attrs = {"class": "col-4"})
-    for panel in panels:
-        print(panel.find("a").get("href"))
+
+    re_inx = re.compile(r"/a_(?P<inx>[0]*)/")
+    dt = np.dtype([('inx', np.int), ('url', np.unicode_, 512), ('name', np.unicode_, 256)])
+    data = np.empty(len(panels), dtype = dt)
+    
+    for (i, panel) in enumerate(panels):
+        element_a = panel.find("a")
+        element_img = panel.find("img")
+        inx_match = re_inx.search(element_a.get("href"))
+        # pdb.set_trace()
+        data[i] = (inx_match.group("inx") if inx_match else 0,
+                   element_img.get("src"),
+                   element_img.get("alt"))
+    print(data)
         
 def main():
     """
