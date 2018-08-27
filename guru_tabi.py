0be35b7eee1e10884652e54d8b44666cf5d99685
Naoya Yamashita <conao3@gmail.com>
2018-08-27 20:20:35 +0900

    create parsePage function

--- guru_tabi.py
+++ guru_tabi.py
@@ -7,15 +7,21 @@ from bs4 import BeautifulSoup
 guru_tabi is twitter-bot for guru-tabi(https://gurutabi.gnavi.co.jp/a/).
 """
 
+def parsePage(r):
+    soup = BeautifulSoup(r.text, 'lxml')
+    
 def fetchPage():
     target_url = 'https://gurutabi.gnavi.co.jp/a/'
     r = requests.get(target_url)
+    return r
 
 def main():
     """
     entry point for guru_tabi
     """
-    fetchPage()
+
+    r = fetchPage()
+    parsePage(r)
     
 if __name__ == '__main__':
     print("guru_tabi v0.1")
