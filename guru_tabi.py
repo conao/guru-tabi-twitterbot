e157fbd419a7a14b3aa16a6df316e4e4de18bdcf
Naoya Yamashita <conao3@gmail.com>
2018-08-27 20:14:36 +0900

    create fetchPage function

--- guru_tabi.py
+++ guru_tabi.py
@@ -7,12 +7,15 @@ from bs4 import BeautifulSoup
 guru_tabi is twitter-bot for guru-tabi(https://gurutabi.gnavi.co.jp/a/).
 """
 
+def fetchPage():
+    target_url = 'https://gurutabi.gnavi.co.jp/a/'
+    r = requests.get(target_url)
+
 def main():
     """
     entry point for guru_tabi
     """
-    target_url = '***'
-    r = requests.get(target_url)
+    fetchPage()
     
 if __name__ == '__main__':
     print("guru_tabi v0.1")
