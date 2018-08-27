7a109cd2b065834a08b0b37fa9285b06e20aa8a1
Naoya Yamashita <conao3@gmail.com>
2018-08-27 20:36:41 +0900

    integrated {fetch, parse}Page function

--- guru_tabi.py
+++ guru_tabi.py
@@ -7,24 +7,20 @@ from bs4 import BeautifulSoup
 guru_tabi is twitter-bot for guru-tabi(https://gurutabi.gnavi.co.jp/a/).
 """
 
-def parsePage(r):
+def browsePage():
+    target_url = 'https://gurutabi.gnavi.co.jp/a/'
+    r = requests.get(target_url)
     soup = BeautifulSoup(r.text, 'lxml')
     panels = soup.find_all("li", attrs = {"class": "col-4"})
     for panel in panels:
         print(panel.find("a").get("href"))
         
-def fetchPage():
-    target_url = 'https://gurutabi.gnavi.co.jp/a/'
-    r = requests.get(target_url)
-    return r
-
 def main():
     """
     entry point for guru_tabi
     """
 
-    r = fetchPage()
-    parsePage(r)
+    browsePage()
     
 if __name__ == '__main__':
     print("guru_tabi v0.1")
