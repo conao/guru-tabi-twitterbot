7ca6a2a35b698c490b748e15b422de51730c80ea
Naoya Yamashita <conao3@gmail.com>
2018-08-27 20:14:17 +0900

    create main function

--- guru_tabi.py
+++ guru_tabi.py
@@ -7,4 +7,14 @@ from bs4 import BeautifulSoup
 guru_tabi is twitter-bot for guru-tabi(https://gurutabi.gnavi.co.jp/a/).
 """
 
-print("guru_tabi v0.1")
+def main():
+    """
+    entry point for guru_tabi
+    """
+    target_url = '***'
+    r = requests.get(target_url)
+    
+if __name__ == '__main__':
+    print("guru_tabi v0.1")
+
+main()
